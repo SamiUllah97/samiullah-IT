1f613992d0ef9a69aa07ea0d0f4861f176296317
Sami Ullah <devillast97@gmail.com>
2021-03-27 16:32:43 +0500

    updated

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("x*x+3*x-4");
-            Console.WriteLine("(x+y)*z");
-            Console.WriteLine("(x+3*y)/(2*x-y)");
-            Console.WriteLine("1/(x*X+x+3)");
-            Console.WriteLine("(x+y)/7");
-            Console.WriteLine("2*b*c*3");
-            Console.WriteLine("x=(3*y)/(5-z)");
-            Console.WriteLine("z=area*sqrt(area)");
-            Console.WriteLine("(x+32)/(y-32)-(x-2*y)");
-            Console.WriteLine("res = (3*i*j*k + pow(k,9))/(7*i*k-5*sqrt(j)+k)");
+            // Console.WriteLine("x*x+3*x-4");
+            // Console.WriteLine("(x+y)*z");
+            // Console.WriteLine("(x+3*y)/(2*x-y)");
+            // Console.WriteLine("1/(x*X+x+3)");
+            // Console.WriteLine("(x+y)/7");
+            // Console.WriteLine("2*b*c*3");
+            // Console.WriteLine("x=(3*y)/(5-z)");
+            // Console.WriteLine("z=area*sqrt(area)");
+            // Console.WriteLine("(x+32)/(y-32)-(x-2*y)");
+            // Console.WriteLine("res = (3*i*j*k + pow(k,9))/(7*i*k-5*sqrt(j)+k)");
 
         }
     }
